60f4c0b9cd6419f54602da3fb974259dd8fe3fb3
Iker Jauregi <ijauregiarancon@gmail.com>
2024-12-12 12:44:20 +0100

    Listo para montar en el server

--- Web/www/conexion.php
+++ Web/www/conexion.php
@@ -2,9 +2,9 @@
 // conexion.php
 
 // Configuración de la base de datos
-$host = "localhost";  // Dirección del servidor (puede ser una IP si no es local)
-$usuario = "root";    // Nombre de usuario de MySQL
-$contrasena = "";     // Contraseña del usuario de MySQL
+$host = "10.11.0.17";  // Dirección del servidor (puede ser una IP si no es local)
+$usuario = "logsonweb";    // Nombre de usuario de MySQL
+$contrasena = "cibergela2024*";     // Contraseña del usuario de MySQL
 $base_datos = "db_logson";  // Nombre de la base de datos
 
 try {
--- Web/www/config.php
+++ Web/www/config.php
@@ -1,34 +1,33 @@
-<?php
 // Configuración de Keycloak
-$realm = 'SSO';  // Nombre del realm
-$client_id = 'SSO';  // ID del cliente registrado en Keycloak
-$client_secret = 'CtisRoENEcw7MYtIBPl2ahaWGSzz10rI';  // Secreto del cliente
-$redirect_uri = 'http://localhost/LWeb/Web/php/login/callback.php';  // URL de callback
-$redirect_uriLogOut = 'http://localhost/LWeb/Web/php/login/loginUnificado.php'; // URL del logout
+// $realm = 'SSO'; // Nombre del realm
+// $client_id = 'SSO'; // ID del cliente registrado en Keycloak
+// $client_secret = 'CtisRoENEcw7MYtIBPl2ahaWGSzz10rI'; // Secreto del cliente
+// $redirect_uri = 'http://localhost/LWeb/Web/php/login/callback.php'; // URL de callback
+// $redirect_uriLogOut = 'http://localhost/LWeb/Web/php/login/loginUnificado.php'; // URL del logout
 
-// Dirección IP de Keycloak
-$ip = '192.168.18.34:8080'; //Casa
-// URLs de Keycloak
-$keycloak_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/auth';
-$token_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/token';
-$keycloak_logout_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/logout?redirect_uri=' . urlencode($redirect_uriLogOut);
-$users_url = 'http://' . $ip . '/admin/realms/' . $realm . '/users';
+// // Dirección IP de Keycloak
+// $ip = '192.168.18.34:8080'; //Casa
+// // URLs de Keycloak
+// $keycloak_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/auth';
+// $token_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/token';
+// $keycloak_logout_url = 'http://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/logout?redirect_uri=' . urlencode($redirect_uriLogOut);
+// $users_url = 'http://' . $ip . '/admin/realms/' . $realm . '/users';
 
 
 
 // Server
-// <?php
-// // Configuración de Keycloak
-// $realm = 'master';  // Nombre del realm
-// $client_id = 'SSO';  // ID del cliente registrado en Keycloak
-// $client_secret = '0OUwDjXfU7eAdBHF9b89I6rRbxV6xrwv';  // Secreto del cliente
-// $redirect_uri = 'http://10.11.0.17/LWeb/Web/php/login/callback.php';  // URL de callback
-// $redirect_uriLogOut = 'http://10.11.0.17/LWeb/Web/php/login/loginUnificado.php'; // URL del logout
+<?php
+// Dirección IP de Keycloak
+$ip = '10.11.0.18:8443'; // Dirección IP y puerto del servidor Keycloak
+// Configuración de Keycloak
+$realm = 'Logson';  // Nombre del realm
+$client_id = 'SSO';  // ID del cliente registrado en Keycloak
+$client_secret = '85ABiJWQxptYYg0wJCP7NCL7LrFawGRk';  // Secreto del cliente
+$redirect_uri = 'https  ://localhost/LWeb/Web/php/login/callback.php';  // URL de callback
+$redirect_uriLogOut = 'https://localhost/LWeb/Web/php/login/loginUnificado.php'; // URL del logout
 
-// // Dirección IP de Keycloak
-// $ip = '10.11.0.18:8443'; // Dirección IP y puerto del servidor Keycloak
-// // URLs de Keycloak
-// $keycloak_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/auth';
-// $token_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/token';
-// $keycloak_logout_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/logout?redirect_uri=' . urlencode($redirect_uriLogOut);
-// $users_url = 'https://' . $ip . '/admin/realms/' . $realm . '/users';
+// URLs de Keycloak
+$keycloak_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/auth';
+$token_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/token';
+$keycloak_logout_url = 'https://' . $ip . '/realms/' . $realm . '/protocol/openid-connect/logout?redirect_uri=' . urlencode($redirect_uriLogOut);
+$users_url = 'https://' . $ip . '/admin/realms/' . $realm . '/users';
